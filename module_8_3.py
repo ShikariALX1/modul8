e38d35e80abfe89568319d6446c281eb3aacfa12
Александр <aleksanderal.92@mail.ru>
2024-11-01 10:55:19 +0300

    Исправление вызова метода валидации

--- module_8_3.py
+++ module_8_3.py
@@ -1,8 +1,6 @@
 class Car:
     def __init__(self, model, vin, numbers):
         self.model = model
-        self.__vin = vin
-        self.__numbers = numbers
         if self.__is_valid_vin(vin):
            self.__vin = vin
         if self.__is_valid_numbers(numbers):
